3fb0bc52849fe9df92615c10bdb411a8078c780c
amata7 <adrianmata@live.com>
2021-05-27 23:32:26 -0700

    server now serves up index.html

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
